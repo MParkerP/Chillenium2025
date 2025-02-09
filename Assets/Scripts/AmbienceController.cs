6cad6d5f76215e4a49c54f4480fbff3ca4f2933a
Matthew Posey <mparkerposey@outlook.com>
2025-02-09 08:10:31 -0600

    started progress manager

--- Assets/Scripts/AmbienceController.cs
+++ Assets/Scripts/AmbienceController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AmbienceController : MonoBehaviour
@@ -6,22 +7,37 @@ public class AmbienceController : MonoBehaviour
     [SerializeField] private GameObject tarmacImage;
     [SerializeField] private float tarmacTargetY;
     [SerializeField] private float tarmacIncrement;
+    [SerializeField] private float tarmacScaler;
+    private bool isTakingOff = false;
 
     private AudioSource audioSource;
     [SerializeField] private AudioClip takeoff;
+    [SerializeField] private AudioSource brownNoise;
+
+    [SerializeField] private GameObject[] shakyObjects;
+
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(takeoff);
+        StartCoroutine(Takeoff());
     }
 
-    IEnumerator planeTakeOff()
+    IEnumerator Takeoff()
     {
-        audioSource.PlayOneShot(takeoff);
-        while (tarmacImage.transform.position.y > tarmacTargetY)
+        yield return new WaitForSeconds(3);
+        isTakingOff=true;
+        yield return new WaitForSeconds(10);
+        brownNoise.Play();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isTakingOff && tarmacImage.transform.position.y > tarmacTargetY)
         {
-            tarmacImage.transform.position = tarmacImage.transform.position + new Vector3(0, -tarmacIncrement, 0);
+            tarmacImage.transform.position = tarmacImage.transform.position + new Vector3(0, -tarmacIncrement, 0) * tarmacScaler;
+            tarmacScaler += 0.025f;
         }
-        yield return new WaitForSeconds(.01f);
     }
 }
--- Assets/Scripts/ProgressManager.cs
+++ Assets/Scripts/ProgressManager.cs
@@ -5,6 +5,27 @@ public class ProgressManager : MonoBehaviour
     [SerializeField] private GameObject progressBar;
     [SerializeField] private float progressBarIncrement;
 
+    private float timer = 0f;
+
+
+    const int SMALL_BIRD_TIME = 0; bool smallBirdOn = false;
+    const int LARGE_BIRD_TIME = 30; bool largeBirdOn = false;
+    const int MUD_TIME = 60; bool mudOn = false;
+    const int CHECKPOINT1_END = 90; bool check1On = false;
+    const int SEAT_BELT_TIME = 150; bool seatBeltOn = false;
+    const int SIMON_SAYS_TIME = 165; bool simonOn = false;
+    const int PASSWORD_TIME = 285; bool password = false;
+    const int TURBULENCE_TIME = 405; bool turbulenceOn = false;
+    const int FINAL_TIME = 465; bool finalOn = false;
+    const int END_TIME = 645; bool endOn = false;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+    }
+
+
     private void FixedUpdate()
     {
         progressBar.transform.position = progressBar.transform.position + new Vector3(-progressBarIncrement, 0, 0);
